a4adf17e207c5ba91acf4a75776896bbf8a69f7f
Brett Post <post.brettw@gmail.com>
2025-05-13 22:07:29 -0700

    Better Dynamic Images and Weekly Special Cards

--- src/little-lemon-capstone/src/WeeklySpecialCard.js
+++ src/little-lemon-capstone/src/WeeklySpecialCard.js
@@ -3,7 +3,8 @@ import React from 'react';
 const WeeklySpecialCard = ({special}) => {
     return (
         <div key={special.id} className="weeklySpecialCard">
-            <img src={special.image} alt={special.name} className="weeklySpecialImage" />
+            <img src={special.image} alt={special.name} />
+            
             <h3 className="weeklySpecialName">{special.name}</h3>
             <p className="weeklySpecialPrice">{special.price}</p>
             <p className="weeklySpecialDescription">{special.description}</p>
